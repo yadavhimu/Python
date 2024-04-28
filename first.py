4176046c975d38ebc1a3778793089304bd7af16d
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-04-28 19:11:50 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,22 @@
+# print("Hello Himanshu")
+
+# a=1000
+# b=500
+# sum=a-b=
+# print(sum)
+
+# how take input from user
+
+# name = input("name :")
+# print(name)
+
+name = input("name :")
+age = int(input("age :"))
+marks = float(input("marks : "))
+
+print("My name is",name,"ang I am", age,"year old","My marks are",marks)
+
+Marks = input("Marks : ")
+if(Marks >= 90){
+   print("Grade A")
+}
