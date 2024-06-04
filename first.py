168a44d5ec1260071fce63e36104874722b79da3
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-06-04 15:47:11 +0530

    prime

--- first.py
+++ first.py
@@ -230,7 +230,18 @@
 # result = square(4)
 # print(result)
 
-# function problem
+
+# function with multiple parameter
 # def add(numOne, numTwo):
 #     return numOne + numTwo
 # print(add(2,5))
+
+
+# question Polymorphism in function
+# def multiply(p1, p2):
+#     return p1 * p2
+
+# print(multiply(8, 5))
+# print(multiply('a',5))
+# print(multiply(5, 'a'))
+
