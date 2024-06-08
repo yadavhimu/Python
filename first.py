a819b09d3006a07918026f4f4254a0c643d38c22
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-06-08 22:35:12 +0530

    example

--- first.py
+++ first.py
@@ -348,3 +348,39 @@
 
 # print(f(3))
 # print(g(3))
+
+#oops 
+# class Car:
+#     total_car = 0
+
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.__model = model
+#         Car.total_car += 1
+
+#     def get_brand(self):
+#         return self.__brand + " !"
+
+#     def full_name(self):
+#         return f"{self.__brand} {self.__model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+    
+#     @staticmethod
+#     def general_description():
+#         return "Cars are means of transport"
+    
+#     @property
+#     def model(self):
+#         return self.__model
+    
+
+
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+#     def fuel_type():
+#         return "Electric charge"
