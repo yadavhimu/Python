1cac8e7f4d7d48a79a0cd84208d41369adc49a0a
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-07-02 10:54:46 +0530

    questions

--- Practice.py
+++ Practice.py
@@ -2,10 +2,16 @@
 # sliced = x[2:6:2]
 # print(sliced)
 
-x= set()
-s= {4,32,2,2,6}
-s2={3,4,22,1}
-s.remove(2)
-print(s)
-print(4 in s)
-print(s.union(s2))
+# x= set()
+# s= {4,32,2,2,6}
+# s2={3,4,22,1}
+# s.remove(2)
+# print(s)
+# print(4 in s)
+# print(s.union(s2))
+
+# x= tuple(i for i in range(100) if i % 5 == 0)
+# print(x)
+
+x= {i for i in range(100) if i % 5 == 0}
+print(x)
