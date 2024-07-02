2f1aefb622cb1a43b5c1a9ae925841c3fbcad848
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-07-02 10:04:36 +0530

    questions

--- Practice.py
+++ Practice.py
@@ -1,3 +1,11 @@
-x=[0,1,2,3,4,5,6,7,8]
-sliced = x[2:6:2]
-print(sliced)
+# x=[0,1,2,3,4,5,6,7,8]
+# sliced = x[2:6:2]
+# print(sliced)
+
+x= set()
+s= {4,32,2,2,6}
+s2={3,4,22,1}
+s.remove(2)
+print(s)
+print(4 in s)
+print(s.union(s2))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,3 @@
-def print_kwargs(**kwargs):
-#     for key, value in kwargs.items():
-#         print(f"{key}: {value}")
-
-
-# print_kwargs(name="shaktiman", power="lazer")
-# print_kwargs(name="shaktiman")
-# print_kwargs(name="shaktiman", power="lazer", enemy = "Dr. Jackaal")
+x=[0,1,2,3,4,5,6,7,8]
+# sliced = x[2:6:2]
+# print(sliced)
