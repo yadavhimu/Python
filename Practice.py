f2ff924f509937d45fcc5d3724bd298b10fe9620
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-07-02 20:04:55 +0530

    questions

--- Practice.py
+++ Practice.py
@@ -23,19 +23,25 @@
 # r1,r2= func(5,6,7)
 # print(r1,r2)
 
-def func(x,y):
-    print(x,y)
+# def func(x,y):
+#     print(x,y)
 
-pairs = [(1,2),(3,4)]
+# pairs = [(1,2),(3,4)]
 
-for pair in pairs:
-    func(*pair)
+# for pair in pairs:
+#     func(*pair)
 
 
-def func(x,y):
-    print(x,y)
+# def func(x,y):
+#     print(x,y)
 
-pairs = [(1,2),(3,4)]
+# pairs = [(1,2),(3,4)]
 
-for pair in pairs:
-    func(**{'x':2, 'y':5})
+# for pair in pairs:
+#     func(**{'x':2, 'y':5})
+
+# def func (*args, **kwargs):
+#     print(**args)
+
+# func(1,2,3,4,5,one=0,two=1)
+
