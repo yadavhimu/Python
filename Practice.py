42805f4f1f9640b097f5cc65ae02df8ccf1c4998
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-07-12 22:46:22 +0530

    problem

--- Practice.py
+++ Practice.py
@@ -116,19 +116,31 @@
 # else:
 #     print("not valid")
 
-a1 = int(input("Enter number 1 : "))
-a2 = int(input("Enter number 2 : "))
-a3 = int(input("Enter number 3 : "))
-a4 = int(input("Enter number 4 : "))
+# a1 = int(input("Enter number 1 : "))
+# a2 = int(input("Enter number 2 : "))
+# a3 = int(input("Enter number 3 : "))
+# a4 = int(input("Enter number 4 : "))
 
-if(a1>a2 and a1>a3 and a1>a4):
-    print("Gratest number is a1:", a1)
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print("Gratest number is a1:", a1)
 
-elif(a2>a1 and a2>a3 and a2>a4):
-    print("Gratest number is a2:", a2)
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print("Gratest number is a2:", a2)
 
-elif(a3>a1 and a3>a2 and a3>a4):
-    print("Gratest number is a3:", a3)
+# elif(a3>a1 and a3>a2 and a3>a4):
+#     print("Gratest number is a3:", a3)
 
-elif(a4>a1 and a4>a2 and a4>a3):
-    print("Gratest number is a4:", a4)
+# elif(a4>a1 and a4>a2 and a4>a3):
+#     print("Gratest number is a4:", a4)
+
+marks1 = int(input("Enter Marks 1: "))
+marks2 = int(input("Enter Marks 2: "))
+marks3 = int(input("Enter Marks 3: "))
+
+total_percentage = (100*(marks1 + marks2 + marks3))/300
+
+if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+    print("You are passed:",total_percentage)
+
+else:
+    print("You are fail:",total_percentage)
