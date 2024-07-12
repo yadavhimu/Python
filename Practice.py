14a8152706bb45806fd95ecdd8e67f6480db97f2
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-07-12 22:35:43 +0530

    problem

--- Practice.py
+++ Practice.py
@@ -109,9 +109,26 @@
 # print(s)
 # print(len(s))
 
-a = int(input("Enter your age: "))
-if(a>=18):
-    print("Yes")
+# a = int(input("Enter your age: "))
+# if(a>=18):
+#     print("Yes")
 
-else:
-    print("not valid")
+# else:
+#     print("not valid")
+
+a1 = int(input("Enter number 1 : "))
+a2 = int(input("Enter number 2 : "))
+a3 = int(input("Enter number 3 : "))
+a4 = int(input("Enter number 4 : "))
+
+if(a1>a2 and a1>a3 and a1>a4):
+    print("Gratest number is a1:", a1)
+
+elif(a2>a1 and a2>a3 and a2>a4):
+    print("Gratest number is a2:", a2)
+
+elif(a3>a1 and a3>a2 and a3>a4):
+    print("Gratest number is a3:", a3)
+
+elif(a4>a1 and a4>a2 and a4>a3):
+    print("Gratest number is a4:", a4)
