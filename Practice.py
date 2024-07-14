ec8b6810e3335580a343145c6a167b29b75db553
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-07-14 23:19:03 +0530

    problem

--- Practice.py
+++ Practice.py
@@ -159,10 +159,17 @@
 #         print(f"Hello {name}")
 
 
-n = int(input("Enter a number:"))
-for i in range(2,n):
-    if(n%i) == 0:
-        print("Number is not prime")
-        break
-else:
-    print("Number is prime")
+# n = int(input("Enter a number:"))
+# for i in range(2,n):
+#     if(n%i) == 0:
+#         print("Number is not prime")
+#         break
+# else:
+#     print("Number is prime")
+
+n = int(input("Enter the number: "))
+product = 1
+for i in range(1, n+1):
+    product = product * i
+
+print(f"The factorial of {n} is {product}")
