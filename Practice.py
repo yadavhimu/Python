ac9851688053dc70fd87c16c8d8754ba96fbdba8
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-07-31 17:41:54 +0530

    problem

--- Practice.py
+++ Practice.py
@@ -178,6 +178,8 @@
 #     print(" "* (n-i), end="")
 #     print("*"* (2*i-1), end="")
 #     print("")
+
+
 # def goodDay(name,ending="Thank you"):
 #     print(f"Good Day, {name}")
 #     print(ending)
