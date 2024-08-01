26d76eda07355f9739d6817c47df95eb1c8703ae
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-08-01 22:12:32 +0530

    problem

--- Practice.py
+++ Practice.py
@@ -172,13 +172,11 @@
 
 # print(f"The factorial of {n} is {product}")
 
-
 # n = int(input("Enter the number: "))
 # for i in range(1, n+1):
 #     print(" "* (n-i), end="")
 #     print("*"* (2*i-1), end="")
 #     print("")
-
 
 # def goodDay(name,ending="Thank you"):
 #     print(f"Good Day, {name}")
